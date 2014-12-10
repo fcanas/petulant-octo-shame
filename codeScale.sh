36c16220b639c7aebfd7c8ecf83309f194b2c138
Fabian Canas <fcanas@gmail.com>
2014-12-10 11:50:55 -0500

    Add Swift to codeScale

--- codeScale.sh
+++ codeScale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Count lines of code in C / Objective-C projects
+# Count lines of code in C / Objective-C / Swift projects
 #
 # Optional argument : the root directory to evaluete. 
 # Defatuls to current directory
-find ${1:-.} "(" -name "*.h" -or -name "*.m" -or -name "*.mm" -or -name "*.hpp" -or -name "*.cpp"  -or  -name "*.c" -or -name "*.cc" ")" -print0 | xargs -0 wc -l
+find ${1:-.} "(" -name "*.h" -or -name "*.m" -or -name "*.mm" -or -name "*.hpp" -or -name "*.cpp"  -or  -name "*.c" -or  -name "*.swift" -or -name "*.cc" ")" -print0 | xargs -0 wc -l
