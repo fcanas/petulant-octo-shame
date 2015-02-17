455cb93f00614bee55f7288103ed6dcb2efd6d3f
Fabian Canas <fcanas@gmail.com>
2015-02-17 12:10:30 -0500

    Add key bindings file, and copy on setup

---
+++ setup/update-env-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ======================
+echo Copying Configurations
+echo ======================
+
+cp -v setup/slate ~/.slate
+cp -v setup/DefaultKeyBinding.dict ~/Library/KeyBindings/
--- setup/update-slate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp setup/slate ~/.slate
