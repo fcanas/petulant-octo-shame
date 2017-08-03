3ac5928f795126dedb3b06e48e3f50d94f862ce2
Fabian Canas <fcanas@gmail.com>
2017-08-03 16:23:19 -0400

    🔨📏📐📱

---
+++ device-support.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport
+open /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport
