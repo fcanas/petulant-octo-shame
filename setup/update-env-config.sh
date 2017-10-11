0c814a65d6685511daaf826941e0f7c134607e96
Fabian Canas <fcanas@twitter.com>
2017-10-11 14:54:47 -0400

    Add line sorting text service

--- setup/update-env-config.sh
+++ setup/update-env-config.sh
@@ -3,9 +3,9 @@ echo ======================
 echo Copying Configurations
 echo ======================
 
-# Not using slate anymore
-# cp -v setup/slate ~/.slate
 mkdir -p ~/bin/
 mkdir -p ~/Library/KeyBindings/ && cp -v setup/DefaultKeyBinding.dict ~/Library/KeyBindings/
+cp -vR 'setup/Sort lines.workflow' ~/Library/Services/
+
 # Not using emacs at the moment
 # cp -v setup/init.el ~/.emacs.d/
